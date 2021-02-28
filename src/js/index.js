7ceaa9c8947ddad0a5f111ad9dd53e5f3dabf984
yancarq <yancarlosrq@gmail.com>
2021-02-28 08:21:06 +0000

    code timer

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,13 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import { ClockFill } from "react-bootstrap-icons";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
+
+import * as Icon from "react-bootstrap-icons";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
@@ -11,5 +15,42 @@ import "../styles/index.scss";
 //import your own components
 import { Home } from "./component/home.js";
 
+const state = {
+	seconds: 0
+};
+let i = 0,
+	b = 0;
+
+setInterval(function() {
+	this.state = {
+		seconds: i++
+	};
+	b = this.state.seconds;
+
+	console.log(this.state.seconds, b);
+}, 1000);
+
+export function SecondsCounter(props) {
+	return (
+		<div className="container">
+			<div className="row d-flex justify-content-center">
+				<div className="clockNumber">
+					<ClockFill />
+				</div>
+				<div className="clockNumber">0</div>
+				<div className="clockNumber">0</div>
+				<div className="clockNumber">0</div>
+				<div className="clockNumber">0</div>
+				<div className="clockNumber">0</div>
+				<div className="clockNumber">0</div>
+			</div>
+		</div>
+	);
+}
+
+SecondsCounter.propTypes = {
+	seconds: PropTypes.Number
+};
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SecondsCounter seconds={0} />, document.querySelector("#app"));
